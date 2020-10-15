50aa21bee77fcb17a5641161f4bb817b45e32f15
Harshit Bagla <harshit@Harshits-MacBook-Pro.local>
2020-10-15 17:13:22 -0500

    Production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase : {
     apiKey: "AIzaSyDjiODbXScXjAAdKR5fKXrXejai6ZbQk4U",
     authDomain: "logos-cd414.firebaseapp.com",
