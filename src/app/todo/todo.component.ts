4d69455732b2ed665a0b037fae0b2e887ba8666b
Harshit Bagla <harshit@Harshits-MacBook-Pro.local>
2020-10-15 15:32:05 -0500

    Add tasks completed

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { TodoService } from './shared/todo.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
+  todos: any[];
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.todoService.getTodos().snapshotChanges().subscribe(item => {
+      this.todos = [];
+      item.forEach(
+        element => {
+          var x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.todos.push(x);
+        }
+      )
+
+      //Sorting todos
+      this.todos.sort((a,b) => {
+        return a.is_completed - b.is_completed;
+        })
+      });
+    }
+
+    onAdd(todoTask): void {
+      this.todoService.addTodos(todoTask.value);
+      todoTask.value = null;
+    
   }
 
 }
