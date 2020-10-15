57b42fffbbe84fc31f2701961b5923a10e06f106
Harshit Bagla <harshit@Harshits-MacBook-Pro.local>
2020-10-15 12:16:41 -0500

    Adding firebase to app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDjiODbXScXjAAdKR5fKXrXejai6ZbQk4U",
+    authDomain: "logos-cd414.firebaseapp.com",
+    databaseURL: "https://logos-cd414.firebaseio.com",
+    projectId: "logos-cd414",
+    storageBucket: "logos-cd414.appspot.com",
+    messagingSenderId: "298427259445",
+    appId: "1:298427259445:web:0640a1dbbfa6da0a5a2f7c",
+    measurementId: "G-PXJM27Z3DD"
+  },
 };
 
 /*
